14f67f2175c5151e59840179299f13d7f00dd274
WojD89 <wojciech.dyrka@gmail.com>
2023-12-18 18:14:30 +0100

    feat: adding fetch form cat api

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-export const fetchCatByBreed = breedId => {
-  return axios.get(`/images/search?breed_ids=${breedId}`);
-};
-//fetch implementation
+// Ustaw klucz API
+axios.defaults.headers.common['x-api-key'] =
+  'live_FGH9lasd0oul4RhK4aaXXVevkdqzPmluPpIdqRAPTVq9GAtoohZ7VPmrFhJI1oqr';
+
 export const fetchBreeds = () => {
-  return axios.get('/breeds').then(response => {
-    return response.data;
-  });
+  return axios.get('https://api.thecatapi.com/v1/breeds');
+};
+
+export const fetchCatByBreed = (breedId) => {
+    
+  return axios.get(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  );
 };
